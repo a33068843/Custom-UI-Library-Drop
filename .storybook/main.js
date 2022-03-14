de73908d5326df0d6c1eed997c1ad489416ca168
Benny Jou <a33068843@gmail.com>
2022-03-14 23:58:55 +0800

    chore: add storybook package

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+const { resolve } = require('path');
+
+module.exports = {
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+  core: {
+    builder: 'storybook-builder-vite',
+  },
+  async viteFinal(config, { configType }) {
+    return {
+      ...config,
+      optimizeDeps: {
+        ...config.optimizeDeps,
+        include: [
+          ...(config.optimizeDeps.include ?? []),
+          '@storybook/react > acorn-jsx',
+        ],
+      },
+    };
+  },
+};
